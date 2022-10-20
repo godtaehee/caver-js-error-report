327fce6b21a85ceb161d907d67011e5917735af6
godtaehee <godtaeheeDev@gmail.com>
2022-10-20 22:19:14 +0900

    Modify node_modules/caver-js/types/index.d.ts
    I modify the type file as adding currentProvider:any
    
    After that, It is operated like current~~.js file.
    
    If it is ugly, please recommend other method or suggesstion.

--- reportScript.ts
+++ reportScript.ts
@@ -2,6 +2,7 @@ import Caver from "caver-js";
 
 const currentProviderIsExistedInCaver = () => {
   const caver = new Caver("https://public-node-api.klaytnapi.com/v1/cypress");
+  console.log(caver.currentProvider);
   console.log(caver.currentProvider.disconnect);
 };
 
